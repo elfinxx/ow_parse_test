b686a3f3bcb1f7d9571130d80edf25754147bb7a
bart.lee <bart.lee@kakaocorp.com>
2016-06-23 18:43:10 +0900

    add gitignore

---
+++ src/Main.java
@@ -0,0 +1,83 @@
+import com.sun.deploy.net.URLEncoder;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+        String encodeString = URLEncoder.encode("케이크류", "UTF-8");
+        Document doc = Jsoup.connect("http://masteroverwatch.com/profile/pc/kr/" + encodeString + "-3111").userAgent("Mozilla").get();
+
+//        System.out.println(doc);
+
+        Elements overViewStats = doc.select("body > main.player-header div.header-stats");
+        System.out.println(overViewStats.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        Document doc = Jsoup.connect("https://playoverwatch.com/en-us/career/pc/kr/" + encodeString + "-3111").timeout(0).get();
+
+
+//        Elements overview = doc.select("#highlights-section > div > ul > li > div > div");
+//
+//        System.out.println(overview.size());
+////        for (Element ele : overview) {
+////            System.out.println("-------------------");
+////            System.out.println(ele.select("p").text());
+////            System.out.println(ele.select("h3").text());
+////        }
+//
+//        // #stats-section > div > div.row > div.js-stats.toggle-display.is-active
+//        // #stats-section > div > div.row > div:nth-child(2)
+////        Elements stats = doc.select("#stats-section > div > div.row > div");
+//        for (OWHero owHero : OWHero.values()) {
+//            String selector = "div[data-category-id=" + owHero.getId() + "] table tbody tr";
+//            Elements stats = doc.select(selector);
+//            System.out.println("\n=================");
+//            System.out.println(owHero.toString());
+//            for (Element ele : stats) {
+//                System.out.println("---------------------------");
+//                System.out.println(ele.children().get(0).text());
+//                System.out.println(ele.children().get(1).text());
+////            System.out.println("String " + ele.text() + "Id = \"" + ele.attr("value") + "\";");
+//            }
+//        }
+//
+//
+////        Elements stats = doc.select("#stats-section > div option");
+//
+////        System.out.println(stats);
+////        Elements m = stats.select("div[data-category-id='0x02E0000000000002'");
+////        System.out.println(m.toString());
+
+    }
+}
---
+++ src/OWHero.java
@@ -0,0 +1,36 @@
+/**
+ * Created by raee on 2016. 6. 23..
+ */
+public enum OWHero {
+    ALLSTAT("0x02E00000FFFFFFFF"),
+    Reaper("0x02E0000000000002"),
+    Tracer("0x02E0000000000003"),
+    Mercy("0x02E0000000000004"),
+    Hanzo("0x02E0000000000005"),
+    Torbjörn("0x02E0000000000006"),
+    Reinhardt("0x02E0000000000007"),
+    Pharah("0x02E0000000000008"),
+    Winston("0x02E0000000000009"),
+    Widowmaker("0x02E000000000000A"),
+    Bastion("0x02E0000000000015"),
+    Symmetra("0x02E0000000000016"),
+    Zenyatta("0x02E0000000000020"),
+    Genji("0x02E0000000000029"),
+    McCree("0x02E0000000000042"),
+    Junkrat("0x02E0000000000065"),
+    Zarya("0x02E0000000000068"),
+    Soldier76("0x02E000000000006E"),
+    Lúcio("0x02E0000000000079"),
+    DVa("0x02E000000000007A");
+
+    final private String id;
+
+    OWHero(String id) {
+        this.id = id;
+
+    }
+
+    public String getId() {
+        return this.id;
+    }
+}
